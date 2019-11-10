7869f74cf12432b4ece9a749a57c644760dbf91a
HaoJiang0201 <haojiang0201@gmail.com>
2019-11-10 02:59:30 +0000

    底部区域初步搭建完毕

--- my-app/src/Bottom/BottomArea.js
+++ my-app/src/Bottom/BottomArea.js
@@ -6,7 +6,22 @@ class BottomArea extends Component {
   render() {
     return (
       <div className="bottom_area">
-        bottom_area
+        <div className="bottom_picture">
+          <div className="bottom_text_large">您有疑问需要解答？</div>
+          <div className="bottom_text_small">选择您方便的联系方式，立即与我们联系。</div>
+          <div className="bottom_item">
+            <div className="bottom_item_image item_image_0" />
+            <div className="bottom_item_text">285-5489 Byrne Rd, Burnaby BC</div>
+          </div>
+          <div className="bottom_item">
+            <div className="bottom_item_image item_image_1" />
+            <div className="bottom_item_text">1-778-833-1131</div>
+          </div>
+          <div className="bottom_item">
+            <div className="bottom_item_image item_image_2" />
+            <div className="bottom_item_text">info@fantuan.ca</div>
+          </div>
+        </div>
       </div>
     );
   }
